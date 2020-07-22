8da7cbf92eb8a85f0e52fdde8d8707f9bf2b28ad
brainwithE <haylyzh@gmail.com>
2020-07-23 06:30:18 +0800

    add map to simulate reusable components

--- config/environment.js
+++ config/environment.js
@@ -30,22 +30,24 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
-
+  
   if (environment === 'test') {
     // Testem prefers this...
     ENV.locationType = 'none';
-
+    
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
+    
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
   }
-
+  
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
+  
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiaGF5bHl6aCIsImEiOiJjazc2MDI2dmswMGhnM2RxbWxrdG0wZzE0In0.nC0v6K2Qx5piUkCx9HFcJQ';
 
   return ENV;
 };
